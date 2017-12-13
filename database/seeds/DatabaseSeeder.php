c0e83af3196883ca72579ae45602007b5a38297b
Victor Hugo <vct.bta@gmail.com>
2017-12-12 23:23:19 -0200

    Adiciona seed da tabela usuário com credencial para teste

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UsersTableSeeder::class,
             ProjectsTableSeeder::class,
             StatusTableSeeder::class,
             TasksTableSeeder::class,
