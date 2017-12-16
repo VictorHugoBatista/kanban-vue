5cb87d79e5cce0dd63d7c14f741ba690503d8f3f
Victor Hugo <vct.bta@gmail.com>
2017-12-16 11:23:11 -0200

    Altera o vue para a nova estrutura retornada no endpoint /tasks

--- resources/assets/js/components/kanban.vue
+++ resources/assets/js/components/kanban.vue
@@ -1,11 +1,11 @@
 <template>
    <div>
        <div class="row">
-          <div class="col-md-3" v-for="(tasks, status) in kanban">
+          <div class="col-md-3" v-for="(tasks, status_id) in kanban">
               <div class="panel panel-default">
-                  <div class="panel-heading">{{status}}</div>
-                  <draggable :id="status" class="panel-body" v-model="kanban[status]" :options="dragOptions"  @end="dragTask">
-                      <div :id="task.id" class="panel" v-for="task in tasks">
+                  <div class="panel-heading">{{tasks['status_name']}}</div>
+                  <draggable :id="status_id" class="panel-body" v-model="kanban[status_id]['tasks']" :options="dragOptions"  @end="dragTask">
+                      <div :id="task.id" class="panel" v-for="task in tasks['tasks']">
                           {{task.name}}
                       </div>
                   </draggable>
@@ -46,8 +46,11 @@
             },
             dragTask(event) {
                 if (event.from.id !== event.to.id) {
-                    console.log('atualizar status task [path] = ' + event.item.id);
+                    this.updateTaskStatus(event.item.id, 1);
                 }
+            },
+            updateTaskStatus(taskId, newStatus) {
+                console.log(taskId, newStatus);
             },
         },
     }
